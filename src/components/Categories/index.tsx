5003f13290072480b91a6a80f5de55d0ee467b3a
JoBenEtuk <etukjosiahbenjamin@gmail.com>
2022-07-26 20:31:49 +0100

    feat: categories

--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
 import cls from './Categories.module.scss'
+import img from '../../assets/categories.png'
 
 const Categories = () => {
+	const list = [
+		'Photography',
+		'Collectibles',
+		'Domain Names',
+		'Music',
+		'Sports',
+		'Utility',
+		'Art',
+	]
 	return (
 		<div className='container'>
 			<section className={cls.categories}>
@@ -10,6 +20,22 @@ const Categories = () => {
 					<h2>Categories</h2>
 					<h3>DefiGen.</h3>
 				</header>
+				<div className={cls.categories__main}>
+					<div className={cls.categories__main__left}>
+						<div>
+							<img src={img} height='733' width='460' alt='' />
+						</div>
+						<p>Photography</p>
+					</div>
+
+					<div className={cls.categories__main__right}>
+						<ul>
+							{list.map((item) => (
+								<li key={item}>{item}</li>
+							))}
+						</ul>
+					</div>
+				</div>
 			</section>
 		</div>
 	)
